79016862a8b15f8bbec71de821117aadf314cecc
VladimirSivac <morndes@gmail.com>
2025-02-17 21:35:15 +0200

    main branch commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    /// <summary>
+    /// Компьютерный практикум № 25
+    /// </summary>
+    internal class Program
+    {
+        
+    }
+}
