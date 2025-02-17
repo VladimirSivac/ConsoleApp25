c4c558ec890eee1a0a825fbbc35879ac8ea16fc2
VladimirSivac <morndes@gmail.com>
2025-02-17 21:51:32 +0200

    task-4 done

--- Program.cs
+++ Program.cs
@@ -11,6 +11,19 @@ namespace Project
     /// </summary>
     internal class Program
     {
-        
+        static void Main(string[] args)
+        {
+            //task4
+            int[] arr = new int[6];
+            Random rand = new Random();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rand.Next(-20, 20);
+            }
+            foreach (var element in arr)
+            {
+                Console.Write($"{element}\t");
+            }
+        }
     }
 }
