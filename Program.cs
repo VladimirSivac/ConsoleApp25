2fcb445b8b0a46c3f56ab12bcbb63cc1be34af35
VladimirSivac <morndes@gmail.com>
2025-02-17 21:39:44 +0200

    task-1 done

--- Program.cs
+++ Program.cs
@@ -11,6 +11,29 @@ namespace Project
     /// </summary>
     internal class Program
     {
-        
+        static void Main(string[] args)
+        {
+            // task1
+            /*int n = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[n];
+            for(int i = 0; i<n; i++)
+            {
+                Console.Write($"arr[{i}]= ");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for(int j=0; j<n; j++)
+            {
+                Console.WriteLine($"arr[{i}]={arr[i]}");
+            }*/
+            Console.Write("n= ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"arr[{i}]= ");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"arr[{i}]={arr[i]}");
+            }
+        }
     }
 }
